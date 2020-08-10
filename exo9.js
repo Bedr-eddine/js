7e2705a82be8b6ccbcf45d010ed46b2b0c45d9b4
unknown <b.badredine@yahoo.fr>
2020-08-10 15:20:55 +0200

    solution step 9

---
+++ exo9.js
@@ -0,0 +1,14 @@
+function convert(nbr) {
+
+    if (nbr > 1000)
+        console.log("vive le Javascript");
+    else {
+        nbr = nbr.toString();
+        for (let i = 0; i < nbr.length; i++) {
+            console.log(nbr.charCodeAt(i));
+        }
+
+    }
+
+}
+convert(100);
