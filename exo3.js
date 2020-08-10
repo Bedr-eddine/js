6fb0b3c795125379fe97ca7268ff6e666ed81d8f
unknown <b.badredine@yahoo.fr>
2020-08-10 10:51:01 +0200

    solution step 3

---
+++ exo3.js
@@ -0,0 +1,9 @@
+function answer(param) {
+    if (param <= 0) {
+        console.log("enfantillage!");
+    } else if (param == 42) {
+        console.log("the answer");
+    } else {
+        console.log("Je peux aller en boite");
+    }
+}
